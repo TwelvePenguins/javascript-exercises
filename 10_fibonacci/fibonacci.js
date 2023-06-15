44aaddfadfa1922270f254949f55494a8ea4f5ae
TwelvePenguins <yuhan.du@gmail.com>
2023-06-16 00:46:02 +0800

    solve js 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibonacciArray = [1, 1]
 
+    if (+index <= 0) { return "OOPS" }
+
+    for (let i = 0; i <= +index; i++) {
+        fibonacciArray.push(fibonacciArray[i] + fibonacciArray[i + 1]);
+    }
+
+    return fibonacciArray[index - 1]
 };
 
 // Do not edit below this line
