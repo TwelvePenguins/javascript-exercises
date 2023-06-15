cc2bdb615c82b836d85364e194ca842b87aef95b
TwelvePenguins <yuhan.du@gmail.com>
2023-06-15 11:41:30 +0800

    solve js 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let result = ""
 
+    if (times >= 0) {
+        for (i = 1; i <= times; i++) {
+            result += string;
+        }
+    } else {
+        result = "ERROR"
+    }
+    return result
 };
 
 // Do not edit below this line
