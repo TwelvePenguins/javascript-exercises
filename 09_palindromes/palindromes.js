9a8c68c41fa7871f69c9885bc4ab4b27ba8f58fa
TwelvePenguins <yuhan.du@gmail.com>
2023-06-15 23:37:18 +0800

    solve js 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let string1 = string.toLowerCase().replace(/\W/g, ""); 
+    return string1.split("").reverse().join("") === string1;
 };
 
 // Do not edit below this line
