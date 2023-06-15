9b3dedb3122c82e09b6b7fa2d96082178f7a173a
TwelvePenguins <yuhan.du@gmail.com>
2023-06-15 11:58:33 +0800

    solve js 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // const inputArray = Array.from(input);
+    // const reversedArray = inputArray.reverse()
+    // let output = "";
+    
+    // reversedArray.join("")
 
+    return Array.from(input).reverse().join("")
 };
 
 // Do not edit below this line
