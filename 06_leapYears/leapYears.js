882d900dba9a3ae6e763ee03e5c481c8cc067e87
TwelvePenguins <yuhan.du@gmail.com>
2023-06-15 14:15:59 +0800

    solve js 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(input) {
+    if (input % 4 === 0) {
+        if (input % 100 === 0 && input % 400 !== 0) {
+            return false
+        } else {
+            return true
+        }
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
