5666fea9623dac6ebc312a2c6f9170d127bcac66
TwelvePenguins <yuhan.du@gmail.com>
2023-06-15 12:34:44 +0800

    solve js 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, target) {
+    let targetIndex = 0;
 
+    for (let i = 0; i < (arguments.length - 1); ++i) {
+
+        if (array.indexOf(arguments[i + 1]) >= 0) {
+            targetIndex = array.indexOf(arguments[i + 1]);
+            array.splice(targetIndex, 1);
+        }
+    }
+    
+    return array
 };
 
 // Do not edit below this line
