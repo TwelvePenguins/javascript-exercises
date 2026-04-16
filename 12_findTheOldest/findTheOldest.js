367b7c6f20740e3ce8e79605da83f03fc2c2101c
TwelvePenguins <yuhan.du@gmail.com>
2026-04-16 21:36:16 +0800

    solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function findTheOldest(array) {
+    array.forEach(person => {
+        if (person.yearOfDeath != undefined) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            let year = new Date().getFullYear()
+            person.age = year - person.yearOfBirth
+        };
+    });
 
+    return array.reduce((oldestPerson, thisPerson) => {
+        return oldestPerson.age < thisPerson.age ? thisPerson : oldestPerson;  
+    });
 };
 
 // Do not edit below this line
