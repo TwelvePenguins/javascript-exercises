0585e0a46eb639036ddd8bb3bfee34b2ee37a61f
TwelvePenguins <yuhan.du@gmail.com>
2026-06-13 21:53:33 +0800

    complete cs 02

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,17 @@
-const contains = function() {
-  
+const contains = function(obj, target) {
+    console.log(Object.keys(obj))
+    const values = Object.values(obj);
+    const immediateObejcts = values.filter((value) => typeof value == "object" && value !== null);
+    console.log(immediateObejcts)
+
+    
+    if (values.includes(target)) {
+        return true;
+    } else if (immediateObejcts.length === 0) {
+        return false;
+    } else {
+        return immediateObejcts.some((object) => contains(object, target));
+    }
 };
   
 // Do not edit below this line
