54fe962d26206a672158a7bb8fd3c8f5ac0657f0
TwelvePenguins <yuhan.du@gmail.com>
2026-06-13 21:08:56 +0800

    complete cs 01

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,10 @@
-const factorial = function() {
-
+const factorial = function(input) {
+    
+    if (input === 1 || input === 0) {
+        return 1;
+    } else if (input > 0 && Number.isInteger(input)){
+        return input * factorial(input - 1);
+    }
 };
 
 // Do not edit below this line
