8035d07b327bcbbffe9127e569eb106a02a8a908
TwelvePenguins <yuhan.du@gmail.com>
2026-06-13 22:57:08 +0800

    complete cs 03

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,34 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data) {
+    let counter = 0;
+    let valuesInArray;
+    if (Array.isArray(data)) {
+        valuesInArray = data;
+    } else if (typeof data === "object") {
+        valuesInArray = Object.values(data);
+    } else {
+        return;
+    }
+
+    counter = valuesInArray.reduce(
+        (accumulator, current) =>
+            Number.isInteger(current) ? (accumulator += 1) : accumulator,
+        0,
+    );
+
+    const nestedStructures = valuesInArray.filter(
+        (element) => typeof element == "object" && element !== null,
+    );
+
+    if (nestedStructures.length === 0) {
+        return counter;
+    } else {
+        nestedStructures.forEach((element) => {
+            counter += totalIntegers(element);
+        });
+
+        return counter;
+    }
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
