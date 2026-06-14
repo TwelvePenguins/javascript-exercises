556eb42cb9f5eeca68e42b39db0e49ef8fbed069
TwelvePenguins <yuhan.du@gmail.com>
2026-06-14 16:25:44 +0800

    complete cs 04

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,33 @@
-const permutations = function() {
-  
+const factorial = function (input) {
+    if (input === 1 || input === 0) {
+        return 1;
+    } else if (input > 0) {
+        return input * factorial(input - 1);
+    }
 };
-  
+
+const permutations = function (array, index = 0, currentArrays = [[]]) {
+    let resultArrays = [];
+    let maximum = array.length;
+
+    if (array.length === 0) {
+        return [[]];
+    }
+
+    currentArrays.forEach((thisArr) => {
+        for (let i = 0; i <= index; i++) {
+            let copy = thisArr.slice();
+            copy.splice(i, 0, array[index]);
+            resultArrays.push(copy);
+        }
+    });
+
+    if (resultArrays.length === factorial(maximum)) {
+        return resultArrays;
+    } else {
+        return permutations(array, index + 1, resultArrays);
+    }
+};
+
 // Do not edit below this line
 module.exports = permutations;
